53f55bffdc6878b2bb1547e32163d05b247293dd
isaque-patricio <isaquem18@gmail.com>
2022-06-18 23:40:35 -0300

    heroku

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,7 +5,7 @@ export default class DB {
   init() {
     return mysql.createConnection({
       host: 'localhost',
-      user: 'root',
+      user: process.env.DB_USERNAME,
       database: 'ASKS',
       password: process.env.DB_PASSWORD
     });
